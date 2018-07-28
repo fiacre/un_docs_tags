38ee7857510501f086864d81845dc48a1570b481
Andrew Lee <fiacre.patrick@gmail.com>
2018-07-28 18:18:41 -0400

    use MultinomialNB, train_test_split utility

--- trainer/load_feature_vectors.py
+++ trainer/load_feature_vectors.py
@@ -2,62 +2,58 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
 from models.models import Document, Tag, DocumentsTags
 from sqlalchemy.sql.expression import func
 # from sqlalchemy.sql import text
-from db.db import get_session, connection
+from db.db import get_session
 from collections import defaultdict
+from math import floor
 
 session = get_session()
 
 v = CountVectorizer(stop_words='english')
-conn = connection()
 
 
-def random_feature_vector(num_docs):
+def random_feature_vectors(num_docs):
     '''
     Supervised learning algorithms will require a category label for each document
     in the training set. In this case the category is the document symbol
     '''
-    train_size = int(num_docs * 0.8)
     doc_query = session.query(Document).order_by(func.random()).limit(num_docs).all()
-    X_train = v.fit_transform([d.raw_text for d in doc_query[0:train_size]])
-    X_test = v.fit_transform([d.raw_text for d in doc_query[train_size + 1:-1]])
+    X = v.fit_transform([d.raw_text for d in doc_query])
 
-    return X_train, X_test
+    #  X_test = v.fit_transform([d.raw_text for d in doc_query[train_size:num_docs]])
+    Y = _get_test_train_tags(doc_query)
+
+    return train_test_split(X, Y, 0.25)
 
 
-def get_test_train_tags(doc_query, train_size):
+def _get_test_train_tags(doc_query, train_size, num_docs):
     symbols_tags = defaultdict(list)
     tags = set()
-    q = doc_query[0:train_size]
-    for doc in q:
-        res = session.query(Tag.representation).join(DocumentsTags). \
+    for doc in doc_query:
+        res = session.query(Tag.tag).join(DocumentsTags). \
             filter(DocumentsTags.c.document_id == doc.id, DocumentsTags.c.tag_id == Tag.id).all()
         for elem in res:
             if len(elem):
                 symbols_tags[doc.symbol].append(elem[0])
                 tags.update(elem)
-
-    test_symbols_tags = defaultdict(list)
-    test_tags = set()
-    q = doc_query[train_size + 1: -1]
-    for doc in q:
-        res = session.query(Tag.representation).join(DocumentsTags). \
-            filter(DocumentsTags.c.document_id == doc.id, DocumentsTags.c.tag_id == Tag.id).all()
-        for elem in res:
-            if len(elem):
-                test_symbols_tags[doc.symbol].append(elem[0])
-                test_tags.update(elem)
+            else:
+                symbols_tags[doc.symbol].append('')
 
     mlb = MultiLabelBinarizer(classes=list(tags))
-    Y_train = mlb.fit_transform(symbols_tags.values())
+    Y = mlb.fit_transform(symbols_tags.values())
+    return Y
 
-    mlb_test = MultiLabelBinarizer(classes=list(test_tags))
-    Y_test = mlb_test.fit_transform(test_symbols_tags.values())
 
-    return Y_test, Y_train
+def train_test_model(X_train, Y_train, X_test, Y_test):
+    mnb = OneVsRestClassifier(MultinomialNB())
+    mnb.fit(X_train, Y_train).predict(X_train)
+    print('Accuracy testing : {:.3f}'.format(mnb.score(X_test, Y_test)))
+    return mnb
 
 
 def get_top_n_words(corpus, n=None):
