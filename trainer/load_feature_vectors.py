9cafb8c3ab9cb429dc8d6b694b83c4fdc1278ef2
Andrew Lee <fiacre.patrick@gmail.com>
2018-08-03 03:31:49 -0400

    getting all the matrix shapes correct

--- trainer/load_feature_vectors.py
+++ trainer/load_feature_vectors.py
@@ -1,20 +1,18 @@
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import LinearSVC
+# from sklearn.svm import LinearSVC
+# from sklearn.pipeline import Pipeline
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from models.models import Document, Tag, DocumentsTags
 from sqlalchemy.sql.expression import func
 # from sqlalchemy.sql import text
 from db.db import get_session
 from collections import defaultdict
-from math import floor
 
 session = get_session()
-
-v = CountVectorizer(stop_words='english')
 
 
 def random_feature_vectors(num_docs):
@@ -22,30 +20,39 @@ def random_feature_vectors(num_docs):
     Supervised learning algorithms will require a category label for each document
     in the training set. In this case the category is the document symbol
     '''
+    v = CountVectorizer(stop_words='english')
     doc_query = session.query(Document).order_by(func.random()).limit(num_docs).all()
-    X = v.fit_transform([d.raw_text for d in doc_query])
+    x = np.array([d.raw_text for d in doc_query])
+    X = v.fit_transform(x)
 
     #  X_test = v.fit_transform([d.raw_text for d in doc_query[train_size:num_docs]])
     Y = _get_test_train_tags(doc_query)
 
-    return train_test_split(X, Y, 0.25)
+    # returns X_train, X_test, y_train, y_test
+    return train_test_split(X, Y, test_size=0.33)
 
 
-def _get_test_train_tags(doc_query, train_size, num_docs):
+def _get_test_train_tags(doc_query):
     symbols_tags = defaultdict(list)
     tags = set()
+    i = 0
     for doc in doc_query:
+        i += 1
         res = session.query(Tag.tag).join(DocumentsTags). \
             filter(DocumentsTags.c.document_id == doc.id, DocumentsTags.c.tag_id == Tag.id).all()
-        for elem in res:
-            if len(elem):
+        if len(res):
+            for elem in res:
                 symbols_tags[doc.symbol].append(elem[0])
                 tags.update(elem)
-            else:
-                symbols_tags[doc.symbol].append('')
+        else:
+            # there are some documents with no tags
+            symbols_tags[doc.symbol].append('NONE')
+            tags.update(['NONE'])
 
+    print("{} docs".format(i))
     mlb = MultiLabelBinarizer(classes=list(tags))
-    Y = mlb.fit_transform(symbols_tags.values())
+    y = np.array(list(symbols_tags.values()))
+    Y = mlb.fit_transform(y)
     return Y
 
 
